02d117bfd245a98b4440c91f81ae621e28bd2c65
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-12-26 20:05:16 +0530

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,14 +1,20 @@
 
 /*
 Usage:
+const { upload } = require('youtube-videos-uploader');
+
+// recoveryemail is optional, only required to bypass login with recovery email if prompted for confirmation
 const credentials = { email: 'email', pass: 'pass', recoveryemail: 'recoveryemail' }
 
-const playlist = { create: true, name: 'arabic lang' }
+// minimum required options to upload video
+const video1 = { path: 'video1.mp4', title: 'title 1', description: 'description 1' }
 
-const video1 = { path: '100.mp4', title: 'title 4', language: 'arabic', tags: ['helo', 'moto'], description: 'description 1', playlist: playlist }
+//set create to true, if playlist doesn't exist
+const playlist = { create: true, name: 'my playlist' }
+// Extra options like tags, language, playlist etc
+const video2 = { path: 'video2.mp4', title: 'title 2', description: 'description 2', language: 'english', tags: ['video', 'github'], playlist: playlist }
 
-const video2 = { path: '100.mp4', title: 'title 5', description: 'description' }
-
+// Returns uploaded video links in array
 upload (credentials, [video1, video2]).then(console.log)
 
 */
