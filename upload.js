701edba37e73e06a2d0a171d2e198fc318165938
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-12-29 12:47:03 +0530

    Wait for processing to start

--- upload.js
+++ upload.js
@@ -157,6 +157,8 @@ async function uploadVideo (videoJSON) {
   await fileChooser.accept([pathToFile])
   // Wait for upload to complete
   await page.waitForXPath('//*[contains(text(),"Upload complete")]', { timeout: 0 })
+  // Wait for upload to go away and processing to start
+  await page.waitForXPath('//*[contains(text(),"Upload complete")]', { hidden: true, timeout: 0 })
   // Wait until title & description box pops up
   await page.waitForFunction('document.querySelectorAll(\'[id="textbox"]\').length > 1')
   const textBoxes = await page.$x('//*[@id="textbox"]')
