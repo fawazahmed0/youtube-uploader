ec957aa2b9a6b1875b00299fe7ed69c99f054062
sacredrel1c <tpek07@gmail.com>
2022-07-07 21:12:15 +0300

    add hidden flag

--- src/upload.ts
+++ src/upload.ts
@@ -165,7 +165,7 @@ async function uploadVideo(videoJSON: Video) {
     }).catch(() => {});
     
     // Wait for upload to complete
-    await page.waitForXPath('//*[contains(text(),"Video upload complete")]',{ timeout: 0 });
+    await page.waitForXPath('//*[contains(text(),"Video upload complete")]',{ hidden: true, timeout: 0 });
     if (videoJSON.onProgress) {
         progress = { progress: 0, stage: ProgressEnum.Processing }
         videoJSON.onProgress(progress)
