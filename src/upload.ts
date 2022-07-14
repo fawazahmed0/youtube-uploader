8dc10eb729635ea72c984b60fa9532be0e693d10
sacredrel1c <tpek07@gmail.com>
2022-07-14 23:33:54 +0300

    fixed langMenuItemSelector after some changes on youtube

--- src/upload.ts
+++ src/upload.ts
@@ -727,7 +727,7 @@ async function changeHomePageLangIfNeeded(localPage: Page) {
     await localPage.click(avatarButtonSelector)
 
     const langMenuItemSelector =
-        '#sections > yt-multi-page-menu-section-renderer:nth-child(3)>#items>ytd-compact-link-renderer>a'
+        '#sections>yt-multi-page-menu-section-renderer:nth-child(3)>#items>ytd-compact-link-renderer>a'
     try {
         await localPage.waitForSelector(langMenuItemSelector)
     } catch (e: any) {
